e077ef458e5aeb49e83b53a0042cadf0d0574e0c
Firman <firmanib@gmail.com>
2023-08-24 00:48:08 +0700

    I'm a Life

---
+++ app/components/navbar/Logo.tsx
@@ -0,0 +1,17 @@
+"use client"
+
+import Image from "next/image";
+import { useRouter } from "next/navigation";
+const Logo = () => {
+    const router = useRouter();
+    return (
+        <Image
+            alt="Logo"
+            className="hidden md:block cursor-pointer"
+            height="40"
+            width="40"
+            src="/images/logo.jpg"
+        />
+    )
+}
+export default Logo;
---
+++ app/layout.tsx
@@ -0,0 +1,44 @@
+import './globals.css'
+import type { Metadata } from 'next'
+import { Nunito } from 'next/font/google'
+import Navbar from './components/navbar/Navbar'
+import ClientOnly from './components/ClientOnly'
+import RegisterModal from './components/modals/RegisterModal'
+import ToasterProvider from './providers/ToasterProvider'
+import LoginModal from './components/modals/LoginModal'
+import SearchModal from './components/modals/SearchModal'
+import RentModal from './components/modals/RentModal'
+import getCurrentUser from './actions/getCurrentUser'
+
+const inter = Nunito({ subsets: ['latin'] })
+
+export const metadata: Metadata = {
+  title: 'Coifree',
+  description: 'Future Ads Media',
+}
+
+export default async function RootLayout({
+  children,
+}: {
+  children: React.ReactNode
+}) {
+  const currentUser = await getCurrentUser();
+  return (
+    <html lang="en">
+      <body className={inter.className}>
+      <ClientOnly>
+          <ToasterProvider />
+          <LoginModal />
+          <RegisterModal />
+          <SearchModal />
+          <RentModal />
+          <Navbar currentUser={currentUser} />
+        </ClientOnly>
+        <div className="pb-20 pt-28">
+          {children}
+        </div>
+      </body>
+    </html>
+
+  )
+}
